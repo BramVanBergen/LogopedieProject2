6cf1cf918a1ac6db85251ffe740c1c633bb41eaa
Bram Van Bergen <r0656050@student.thomasmore.be>
2018-12-16 14:41:54 +0100

    Voor merge

--- app/src/main/java/be/thomasmore/logopedieproject2/MainActivity.java
+++ app/src/main/java/be/thomasmore/logopedieproject2/MainActivity.java
@@ -1,32 +1,61 @@
 package be.thomasmore.logopedieproject2;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    private int year, month, day;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        EditText geboortedatum = (EditText) findViewById(R.id.geboortedatum);
+        geboortedatum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showDatePickerDialog();
+            }
+        });
+
+        geboortedatum.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    showDatePickerDialog();
+                }
             }
         });
     }
+
+    public void showDatePickerDialog() {
+        DatePickerDialog datepicker = new DatePickerDialog(this, mDateSetListener, year, month, day);
+        datepicker.setTitle(getString(R.string.Geboortedatumlabel));
+        datepicker.show();
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener(){
+        public void onDateSet (DatePicker view, int newYear, int newMonth, int newDay) {
+            year = newYear;
+            month = newMonth;
+            day = newDay;
+
+            TextView textViewGeboortedatum = (TextView) findViewById(R.id.geboortedatum);
+            textViewGeboortedatum.setText(day + "/" + (month + 1) + "/" + year);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
