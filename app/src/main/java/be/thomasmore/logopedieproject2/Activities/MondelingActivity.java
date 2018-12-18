c82134498b6867504cb3f9617025ee163a861957
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-12-18 09:39:51 +0100

    gitignore

---
+++ app/src/main/java/be/thomasmore/logopedieproject2/Activities/MondelingActivity.java
@@ -0,0 +1,24 @@
+package be.thomasmore.logopedieproject2.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import be.thomasmore.logopedieproject2.R;
+
+public class MondelingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mondeling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Mondelinge beschrijving");
+
+    }
+
+}
