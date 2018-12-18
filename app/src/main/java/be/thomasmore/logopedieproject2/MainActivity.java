5c75ce622ce48cd50c995990df56587996b3c121
Bram Van Bergen <r0656050@student.thomasmore.be>
2018-12-18 10:20:51 +0100

    PatientActivity

--- app/src/main/java/be/thomasmore/logopedieproject2/MainActivity.java
+++ app/src/main/java/be/thomasmore/logopedieproject2/MainActivity.java
@@ -3,7 +3,6 @@ package be.thomasmore.logopedieproject2;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.InputType;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,25 +12,20 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
-import be.thomasmore.logopedieproject2.DataService.PatientDataService;
-import be.thomasmore.logopedieproject2.DataService.ScoreDataService;
+import be.thomasmore.logopedieproject2.Activities.PatientActivity;
 import be.thomasmore.logopedieproject2.DataService.SoortAfasieDataService;
 import be.thomasmore.logopedieproject2.Models.SoortAfasie;
 
 
 public class MainActivity extends AppCompatActivity {
-    Calendar calendar = Calendar.getInstance();
-    private SoortAfasieDataService dbAfasie;
-    private int year = calendar.get(Calendar.YEAR), month = calendar.get(Calendar.MONTH), day = calendar.get(Calendar.DATE);
 
-    PatientActivity PA = new PatientActivity();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,67 +33,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-        //DATEPICKER GEBOORTEDATUM + SET CHRONOLOGISCHE LEEFTIJD
-        final EditText geboortedatum = (EditText) findViewById(R.id.geboortedatum);
-        geboortedatum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDatePickerDialogGeboortedatum();
-            }
-        });
-
-        geboortedatum.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                if (b) {
-                    showDatePickerDialogGeboortedatum();
-                }
-            }
-        });
-
-        //DATEPICKER TESTDATUM + SET CHRONOLOGISCHE LEEFTIJD
-        EditText testdatum = (EditText) findViewById(R.id.testdatum);
-        testdatum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDatePickerDialogTestdatum();
-            }
-        });
-
-        testdatum.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                if (b) {
-                    showDatePickerDialogTestdatum();
-                }
-            }
-        });
-
-        //SOORTEN AFASIE
-        // Spinner Drop down elements
-        dbAfasie = new SoortAfasieDataService(new DatabaseHelper(this));
-        List<SoortAfasie> soortenafasie = dbAfasie.getSoortAfasieList();
-        List<String> SoortenAfasie = new ArrayList<>();
-        for (SoortAfasie soortafasie: soortenafasie)
-        {
-            SoortenAfasie.add(soortafasie.getNaam());
-        }
-
-        // Creating adapter for spinner
-        ArrayAdapter<String> AfasieAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, SoortenAfasie);
-
-        Spinner SoortAfasieSpinner = (Spinner)findViewById(R.id.afasie);
-        SoortAfasieSpinner.setAdapter(AfasieAdapter);
-
-        //BUTTON SUBMIT ONCLICK
-        Button btnSubmit = (Button) findViewById(R.id.buttonsubmit);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PA.onSubmit();
-            }
-        });
     }
 
     public void berekenChronologischeLeeftijd() {
@@ -121,49 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //DATEPICKER GEBOORTEDATUM
-    public void showDatePickerDialogGeboortedatum() {
-        DatePickerDialog datepicker = new DatePickerDialog(this, mDateSetListenerGeboortedatum, year, month, day);
-        datepicker.setTitle(getString(R.string.Geboortedatumlabel));
-        datepicker.show();
-    }
 
-    private DatePickerDialog.OnDateSetListener mDateSetListenerGeboortedatum = new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int newYear, int newMonth, int newDay) {
-            year = newYear;
-            month = newMonth;
-            day = newDay;
-
-            TextView textViewGeboortedatum = (TextView) findViewById(R.id.geboortedatum);
-            textViewGeboortedatum.setText(day + "/" + (month + 1) + "/" + year);
-
-            year = calendar.get(Calendar.YEAR);
-            month = calendar.get(Calendar.MONTH);
-            day = calendar.get(Calendar.DATE);
-        }
-    };
-
-    //DATEPICKER TESTDATUM
-    public void showDatePickerDialogTestdatum() {
-        DatePickerDialog datepicker = new DatePickerDialog(this, mDateSetListenertestdatum, year, month, day);
-        datepicker.setTitle(getString(R.string.Geboortedatumlabel));
-        datepicker.show();
-    }
-
-    private DatePickerDialog.OnDateSetListener mDateSetListenertestdatum = new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int newYear, int newMonth, int newDay) {
-            year = newYear;
-            month = newMonth;
-            day = newDay;
-
-            TextView textViewTestdatum = (TextView) findViewById(R.id.testdatum);
-            textViewTestdatum.setText(day + "/" + (month + 1) + "/" + year);
-
-            year = calendar.get(Calendar.YEAR);
-            month = calendar.get(Calendar.MONTH);
-            day = calendar.get(Calendar.DATE);
-        }
-    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
