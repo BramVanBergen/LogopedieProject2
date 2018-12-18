b6de6f18a6c9994a4bc9d2072886dbca4bc28f28
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-12-18 11:07:24 +0100

    schriftelijke oefening toegevoegd

--- app/src/main/java/be/thomasmore/logopedieproject2/Activities/MondelingActivity.java
+++ app/src/main/java/be/thomasmore/logopedieproject2/Activities/MondelingActivity.java
@@ -1,11 +1,7 @@
 package be.thomasmore.logopedieproject2.Activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import be.thomasmore.logopedieproject2.MenuActivity;
 import be.thomasmore.logopedieproject2.R;
@@ -18,8 +14,7 @@ public class MondelingActivity extends MenuActivity {
         setContentView(R.layout.activity_mondeling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Mondelinge beschrijving");
-
+        getSupportActionBar().setTitle("Mondelinge oefening");
     }
 
 }
--- app/src/main/java/be/thomasmore/logopedieproject2/Activities/SchriftelijkAcitivty.java
+++ app/src/main/java/be/thomasmore/logopedieproject2/Activities/SchriftelijkAcitivty.java
@@ -18,15 +18,8 @@ public class SchriftelijkAcitivty extends MenuActivity {
         setContentView(R.layout.activity_schriftelijk_acitivty);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Schriftelijke oefening");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
